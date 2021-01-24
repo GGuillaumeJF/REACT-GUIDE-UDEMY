1b939f89d73aab8a9f6777b811e78932387d03ec
Guillaume <goffaux.guillaumex@yahoo.fr>
2021-01-24 07:25:10 -0500

    First commit

---
+++ src/Person/Person.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Person.css';
+
+const person = (props) => {
+    return (
+        <div className="Person">
+            <p>I'm a {props.name} and I have {props.age} years old!</p>
+            <p>{props.children}</p>
+        </div>
+    )
+};
+
+export default person;
+
