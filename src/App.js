4dc2d1e81b423b711e628de2e97aaeab35400860
Guillaume <goffaux.guillaumex@yahoo.fr>
2021-02-06 07:43:30 -0500

    Section 69 Using Radium

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import Radium, { StyleRoot } from 'radium';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
 
 class App extends Component {
   state = {
@@ -50,7 +51,11 @@ class App extends Component {
       font : 'inherit',
       border : '1px solid blue',
       padding : '8px',
-      cursor : 'pointer'
+      cursor : 'pointer',
+      ':hover' : {
+        backgroundColor: 'lightgreen',
+        color:'black'
+      }
     };
 
     let persons = null;
@@ -67,63 +72,38 @@ class App extends Component {
             changed = {(event) => this.nameChangedHandler(event,person.id)} />            
           })}
         </div>
-      )
+      );
+      style.backgroundColor = 'red';
+      style[':hover'] = {        
+          backgroundColor: 'salmon',
+          color:'black'
+        }      
+    }
+
+    const classes = [];
+    
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working !</p>
-        <button 
-        style={style}
-        onClick={this.togglePersonsHandler}>Toggle Persons</button> 
-        {persons}              
-      </div>
+
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working !</p>
+          <button 
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button> 
+          {persons}              
+        </div>
+      </StyleRoot>
     );
   }
 }
 
-export default App;
-
-
-// const app = (props) => {
-//   const [personsState, setPersonsState] = useState({
-//     persons: [
-//       { name: 'Max', age: 28 },
-//       { name: 'Manu', age: 29 },
-//       { name: 'Stephanie', age: 26 }
-//     ]
-//   });
-
-//   const [otherState, setOtherState] = useState('some other value');
-
-//   console.log(personsState, otherState);
-
-//   const switchNameHandler = () => {
-//     // console.log('Was clicked!');
-//     // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-//     setPersonsState({
-//       persons: [
-//         { name: 'Maximilian', age: 28 },
-//         { name: 'Manu', age: 29 },
-//         { name: 'Stephanie', age: 27 }
-//       ]
-//     });
-//   };
-
-//   return (
-//     <div className="App">
-//       <h1>Hi, I'm a React App</h1>
-//       <p>This is really working!</p>
-//       <button onClick={switchNameHandler}>Switch Name</button>
-//       <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-//       <Person name={personsState.persons[1].name} age={personsState.persons[1].age}> My Hobbies: Racing </Person>
-//       <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
-//     </div>
-//   );
-//   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
-// };
-
-// export default app;
-
-
+export default Radium(App);
